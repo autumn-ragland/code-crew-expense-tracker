3b23101e4a84cae37839f21d0b6126740c140e50
Autumn Ragland <46455498+autumn-ragland@users.noreply.github.com>
2020-01-30 21:39:40 -0600

    styling and layout

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # code-crew-expense-tracker
+
+Expense tracker to encourage families to engage in conversation about finances. 
--- scripts.js
+++ scripts.js
@@ -1,1 +1,5 @@
 console.log("project loaded")
+function expenseFormSubmissionParent(){
+    console.log(Event.value)
+    console.log(document.getElementById("expenseCategoryParentForm").value)
+}
